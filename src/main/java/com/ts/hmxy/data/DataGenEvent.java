b3a86f778c57c582a3e52cd35d0b8718645a4d6b
TangSoon <1958124144@qq.com>
2022-04-17 02:22:52 +0800

    战利品表codeGen模板

--- src/main/java/com/ts/hmxy/data/DataGenEvent.java
+++ src/main/java/com/ts/hmxy/data/DataGenEvent.java
@@ -1,5 +1,6 @@
 package com.ts.hmxy.data;
 
+import net.minecraft.data.tags.BlockTagsProvider;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.forge.event.lifecycle.GatherDataEvent;
@@ -8,6 +9,10 @@ import net.minecraftforge.forge.event.lifecycle.GatherDataEvent;
 public class DataGenEvent {
     @SubscribeEvent
     public static void dataGen(GatherDataEvent event) {
+    	BlockTagsProvider blockTagsProvider=new HmxyBlockTagsProvider(event.getGenerator(),event.getExistingFileHelper());
         event.getGenerator().addProvider(new HmxyRecipeProvider(event.getGenerator()));
+        event.getGenerator().addProvider(blockTagsProvider);
+        //event.getGenerator().addProvider(new HmxyItemTagsProvider(event.getGenerator(), blockTagsProvider, event.getExistingFileHelper()));
+        event.getGenerator().addProvider(new HmxyLootTableProvider(event.getGenerator()));
     }
 }

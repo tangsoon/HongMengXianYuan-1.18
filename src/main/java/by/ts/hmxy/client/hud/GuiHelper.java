5804b178af7d8a03641bf9cd95f4cd1dbbbf3ddc
Tangsoon <1958124144@qq.com>
2022-04-26 06:25:07 +0800

    chore: 添加创世神相关组件

--- src/main/java/by/ts/hmxy/client/hud/GuiHelper.java
+++ src/main/java/by/ts/hmxy/client/hud/GuiHelper.java
@@ -17,7 +17,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.gui.ForgeIngameGui;
-
+//TODO 合并到HmxyHud类中
 @OnlyIn(Dist.CLIENT)
 public class GuiHelper {
 

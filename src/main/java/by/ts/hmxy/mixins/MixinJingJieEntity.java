d0aa57b2c452af8a2148ed87f541fa5398a94c59
Tangsoon <1958124144@qq.com>
2022-04-29 11:38:28 +0800

    feature:

---
+++ src/main/java/by/ts/hmxy/mixins/MixinJingJieEntity.java
@@ -0,0 +1,44 @@
+package by.ts.hmxy.mixins;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import by.ts.hmxy.util.HmxyHelper;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+
+@Mixin(Player.class)
+public abstract class MixinJingJieEntity extends LivingEntity {
+
+	private final Logger logger = LogManager.getLogger();
+
+	protected MixinJingJieEntity(EntityType<? extends LivingEntity> p_20966_, Level p_20967_) {
+		super(p_20966_, p_20967_);
+		logger.error("调用了一个不该调用的方法: " + this.getClass());
+	}
+
+	@Inject(method = "readAdditionalSaveData", at = @At("RETURN"))
+	public void onReadAdditionalSaveData(CompoundTag pCompound, CallbackInfo ci) {
+		HmxyHelper.setZhenYuan(this, pCompound.getInt("zhenYuan"));
+		HmxyHelper.setXiaoJingJie(this, pCompound.getInt("xiaoJingJie"));
+		HmxyHelper.setLingLi(this, pCompound.getFloat("lingLi"));
+	}
+
+	@Inject(method = "addAdditionalSaveData", at = @At("RETURN"))
+	public void onAddAdditionalSaveData(CompoundTag pCompound, CallbackInfo ci) {
+		pCompound.putInt("zhenYuan", HmxyHelper.getZhenYuan(this));
+		pCompound.putInt("xiaoJingJie", HmxyHelper.getXiaoJingJie(this));
+		pCompound.putFloat("lingLi", HmxyHelper.getLingLi(this));
+	}
+	
+	@Inject(method = "tick", at = @At("RETURN"))
+	public void onTick(CallbackInfo ci) {
+		
+	}
+}

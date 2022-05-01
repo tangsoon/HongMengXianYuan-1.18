b389233f9957ac939d19272fcc873f810124dd8f
Tangsoon <1958124144@qq.com>
2022-05-01 15:51:44 +0800

    fix: 修复注册表冻结

--- src/main/java/by/ts/hmxy/block/HmxyBlocks.java
+++ src/main/java/by/ts/hmxy/block/HmxyBlocks.java
@@ -1,5 +1,6 @@
 package by.ts.hmxy.block;
 
+import java.util.function.Supplier;
 import java.util.function.ToIntFunction;
 import by.ts.hmxy.HmxyMod;
 import net.minecraft.util.valueproviders.UniformInt;
@@ -19,23 +20,23 @@ public class HmxyBlocks {
 			HmxyMod.MOD_ID);
 	/** 灵石矿 */
 	public static final RegistryObject<Block> REIKI_STONE_ORE = register("reiki_stone_ore",
-			new ReikiStoneOreBlock(
+			() -> new ReikiStoneOreBlock(
 					Properties.of(Material.STONE).requiresCorrectToolForDrops().strength(3.0F, 3.0F)
 							.sound(SoundType.DEEPSLATE).randomTicks().lightLevel(litBlockEmission(9)),
 					UniformInt.of(0, 2), ReikiStoneOreBlock.Type.ORDINARY.type));
-	
+
 	/** 闪烁的灵石矿 */
 	public static final RegistryObject<Block> REIKI_STONE_ORE_FLICKER = register("reiki_stone_ore_flicker",
-			new ReikiStoneOreBlock(Properties.copy(REIKI_STONE_ORE.get()), UniformInt.of(0, 2),
+			() -> new ReikiStoneOreBlock(Properties.copy(REIKI_STONE_ORE.get()), UniformInt.of(0, 2),
 					ReikiStoneOreBlock.Type.FLICKER.type));
 
 	/** 凡界传送门，往生泉 */
 	public static final RegistryObject<Block> PREVIOUS_LIFE_WATER = register("previous_life_water",
-			new PreviousLifeWaterBlock());
+			() -> new PreviousLifeWaterBlock());
 
 	/** 灵脉 */
 	public static final RegistryObject<Block> LING_MAI = register("ling_mai",
-			new LingMaiBlock(Properties.copy(Blocks.STONE).noDrops()));
+			() -> new LingMaiBlock(Properties.copy(Blocks.STONE).noDrops()));
 
 	private static ToIntFunction<BlockState> litBlockEmission(int pLightValue) {
 		return (p) -> {
@@ -43,7 +44,8 @@ public class HmxyBlocks {
 		};
 	}
 
-	private static RegistryObject<Block> register(String name, Block block) {
-		return BLOCKS.register(name, () -> block);
+	// 注意Supplier和直接new个的区别，Supplier只有调用的时候才创建对象，而new是直接创建对象
+	private static RegistryObject<Block> register(String name, Supplier<Block> s) {
+		return BLOCKS.register(name, s);
 	}
 }

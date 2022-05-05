69115730424c9b7dad2883916504ddeaa17a93bf
Tangsoon <1958124144@qq.com>
2022-05-05 21:12:46 +0800

    refactor: 重构生物名字生成

--- src/main/java/by/ts/hmxy/data/HmxyLanguageProvider.java
+++ src/main/java/by/ts/hmxy/data/HmxyLanguageProvider.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.BiConsumer;
 import by.ts.hmxy.HmxyMod;
-import by.ts.hmxy.entity.HmxyEntities;
 import by.ts.hmxy.item.Grade;
 import by.ts.hmxy.util.TransMsg;
 import net.minecraft.data.DataGenerator;
@@ -24,6 +23,7 @@ public class HmxyLanguageProvider extends LanguageProvider {
     public static final  Map<RegistryObject<Item>,String> ITEM_NAMES=new HashMap<>();
     public static final Map<Grade,String> GRADE_NAMES=new HashMap<>();
     public static final Map<TransMsg,String> MSG_TEXT=new HashMap<>();
+    public static final Map<String,String> ENTITY_NAME=new HashMap<>();
     
     @Override
     protected void addTranslations() {
@@ -31,7 +31,7 @@ public class HmxyLanguageProvider extends LanguageProvider {
     	this.addRegistryObj(ITEM_NAMES, this::add);
     	this.add(GRADE_NAMES,this::add);
     	this.add(MSG_TEXT, this::add);
-    	this.addEntityType(HmxyEntities.MINBUS_ORB, "灵气");
+    	this.add(ENTITY_NAME, this::add);
     }
     
     public void add(Grade grade, String name) {
--- src/main/java/by/ts/hmxy/entity/HmxyEntities.java
+++ src/main/java/by/ts/hmxy/entity/HmxyEntities.java
@@ -1,6 +1,10 @@
 package by.ts.hmxy.entity;
 
+import java.util.function.Supplier;
+
 import by.ts.hmxy.HmxyMod;
+import by.ts.hmxy.data.HmxyLanguageProvider;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
 import net.minecraft.world.item.Item;
@@ -14,12 +18,18 @@ public class HmxyEntities {
 			HmxyMod.MOD_ID);
 	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, HmxyMod.MOD_ID);
 
-	/**灵气*/
-	public static final RegistryObject<EntityType<MinbusOrb>> MINBUS_ORB = ENTITIES.register("minbus_orb",()->
-			EntityType.Builder.<MinbusOrb>of(MinbusOrb::new, MobCategory.MISC).sized(0.5F, 0.5F)
+	public static final RegistryObject<EntityType<MinbusOrb>> MINBUS_ORB = register("minbus_orb", "灵气",
+			() -> EntityType.Builder.<MinbusOrb>of(MinbusOrb::new, MobCategory.MISC).sized(0.5F, 0.5F)
 					.clientTrackingRange(6).updateInterval(20).build("minbus_orb"));
-	/**扔出的灵气瓶*/
-	public static final RegistryObject<EntityType<ThrownMinbusBottle>> THROWN_MINBUS_BOTTLE = ENTITIES.register("thrown_minbus_bottle",()->
-	EntityType.Builder.<ThrownMinbusBottle>of(ThrownMinbusBottle::new, MobCategory.MISC).sized(0.25F, 0.25F)
-			.clientTrackingRange(6).updateInterval(20).build("thrown_minbus_bottle"));
+	public static final RegistryObject<EntityType<ThrownMinbusBottle>> THROWN_MINBUS_BOTTLE = register(
+			"thrown_minbus_bottle", "扔出的灵气瓶",
+			() -> EntityType.Builder.<ThrownMinbusBottle>of(ThrownMinbusBottle::new, MobCategory.MISC)
+					.sized(0.25F, 0.25F).clientTrackingRange(6).updateInterval(20).build("thrown_minbus_bottle"));
+
+	public static final <T extends Entity> RegistryObject<EntityType<T>> register(String name, String nameZh,
+			Supplier<EntityType<T>> sup) {
+		RegistryObject<EntityType<T>> obj = ENTITIES.register(name, sup);
+		HmxyLanguageProvider.ENTITY_NAME.put("entity."+HmxyMod.MOD_ID+"."+name, nameZh);
+		return obj;
+	}
 }

475eb581c6f11739ec023ccbe57264d1fdb90a16
Tangsoon <1958124144@qq.com>
2022-05-12 14:47:05 +0800

    feature: 删除基因相关

--- src/main/java/by/ts/hmxy/util/gene/IGene.java
+++
@@ -1,6 +0,0 @@
-package by.ts.hmxy.util.gene;
-
-public interface IGene<T> {
-	IGeneType<T> getGenType();
-	T getValue();
-}
--- src/main/java/by/ts/hmxy/util/gene/IGeneType.java
+++
@@ -1,9 +0,0 @@
-package by.ts.hmxy.util.gene;
-
-import net.minecraft.world.level.block.state.properties.Property;
-
-interface IGeneType<T> {
-	String getTypeName();
-	Class<T> valueType();
-	Property<?> getProperty();
-}
